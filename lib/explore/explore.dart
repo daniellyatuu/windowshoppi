984464d6d493fad9a8f4862c735e813f0b70ad91
Lyatuu <daniellyatuu@gmail.com>
2020-07-11 11:21:13 +0300

    load more 2

--- lib/explore/explore.dart
+++ lib/explore/explore.dart
@@ -22,141 +22,70 @@ class Explore extends StatefulWidget {
 }
 
 class _ExploreState extends State<Explore> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'windowshoppi',
-          style: TextStyle(fontFamily: 'Itim'),
-        ),
-        actions: <Widget>[
-          SelectCountry(),
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: FutureBuilder(
-        future: fetchProduct(http.Client(), ALL_PRODUCT_URL),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print(snapshot.error);
-//            Center(
-//              child: Text('display error'),
-//            );
-          }
-          return snapshot.hasData
-              ? SinglePost(product: snapshot.data)
-              : Center(child: CircularProgressIndicator(strokeWidth: 3.0));
-        },
-      ),
-    );
-  }
-}
-
-class SinglePost extends StatefulWidget {
-  final List<Product> product;
-
-  SinglePost({Key key, this.product}) : super(key: key);
-
-  @override
-  _SinglePostState createState() => _SinglePostState();
-}
-
-class _SinglePostState extends State<SinglePost> {
   ScrollController _scrollController = ScrollController();
+  List data;
+  String nextUrl;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    print('check scrolling in here');
-    print(widget.product);
+    this.fetchProduct(http.Client(), ALL_PRODUCT_URL);
 
     _scrollController.addListener(() {
 //      print(_scrollController.position.pixels);
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
-        loadMorePost(nextLink);
-        setState(() {});
+        if (nextUrl != null) {
+          this.fetchProduct(http.Client(), nextUrl);
+        }
 
-//        print(nextLink);
+//        print(nextUrl);
       }
     });
   }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _scrollController.dispose();
-    super.dispose();
-  }
+  Future<List<Product>> fetchProduct(http.Client client, url) async {
+//  final response = await client.get(ALL_PRODUCT_URL);
+    final response = await client.get(url);
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        controller: _scrollController,
-        itemCount: widget.product.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                TopSection(
-                  account: widget.product[index].accountName,
-                  location: widget.product[index].businessLocation,
-                ),
-//                PostSection(postImage: product[index].productPhoto),
-                PostSection(postData: widget.product[index]),
-                BottomSection(
-                    callNo: widget.product[index].callNumber,
-                    whatsapp: widget.product[index].whatsappNumber),
-                PostDetails(caption: widget.product[index].caption),
-              ],
-            ),
-          );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> mapResponse = json.decode(response.body);
+
+      nextUrl = mapResponse['next'];
+      if (mapResponse["count"] != "") {
+        final products = mapResponse["results"].cast<Map<String, dynamic>>();
+        final listOfProducts = await products.map<Product>((json) {
+          return Product.fromJson(json);
+        }).toList();
+//        return listOfProducts;
+
+        setState(() {
+          data = listOfProducts;
         });
+      } else {
+        return [];
+      }
+    } else {
+      throw Exception('failed to load data from internet');
+    }
   }
-}
-
-//Column(
-//children: <Widget>[
-//AppCategory(),
-//Expanded(
-//child: ListView.builder(
-//itemCount: imageList.length,
-//itemBuilder: (context, index) {
-//return SinglePost(imgUrl: imageList[index]);
-//},
-//),
-//),
-//],
-//),
-
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(),
-        borderRadius: BorderRadius.circular(20),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('http get'),
       ),
-      child: Image.network(
-        'https://example.com/image.jpg',
-        loadingBuilder: (BuildContext context, Widget child,
-            ImageChunkEvent loadingProgress) {
-          if (loadingProgress == null) return child;
-          return Center(
-            child: CircularProgressIndicator(
-              value: loadingProgress.expectedTotalBytes != null
-                  ? loadingProgress.cumulativeBytesLoaded /
-                      loadingProgress.expectedTotalBytes
-                  : null,
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Container(
+              height: 200,
+              color: Colors.blue,
+              child: Text(data[index].caption),
             ),
           );
         },
@@ -164,3 +93,152 @@ class MyStatelessWidget extends StatelessWidget {
     );
   }
 }
+
+//class Explore extends StatefulWidget {
+//  @override
+//  _ExploreState createState() => _ExploreState();
+//}
+//
+//class _ExploreState extends State<Explore> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text(
+//          'windowshoppi',
+//          style: TextStyle(fontFamily: 'Itim'),
+//        ),
+//        actions: <Widget>[
+//          SelectCountry(),
+//        ],
+//      ),
+//      drawer: AppDrawer(),
+//      body: FutureBuilder(
+//        future: fetchProduct(http.Client(), ALL_PRODUCT_URL),
+//        builder: (context, snapshot) {
+//          if (snapshot.hasError) {
+//            print(snapshot.error);
+////            Center(
+////              child: Text('display error'),
+////            );
+//          }
+//          return snapshot.hasData
+//              ? SinglePost(product: snapshot.data)
+//              : Center(child: CircularProgressIndicator(strokeWidth: 3.0));
+//        },
+//      ),
+//    );
+//  }
+//}
+//
+//class SinglePost extends StatefulWidget {
+//  final List<Product> product;
+//
+//  SinglePost({Key key, this.product}) : super(key: key);
+//
+//  @override
+//  _SinglePostState createState() => _SinglePostState();
+//}
+//
+//class _SinglePostState extends State<SinglePost> {
+//  ScrollController _scrollController = ScrollController();
+//
+//  @override
+//  void initState() {
+//    // TODO: implement initState
+//    super.initState();
+//    print('check scrolling in here');
+//    print(widget.product);
+//
+//    _scrollController.addListener(() {
+////      print(_scrollController.position.pixels);
+//      if (_scrollController.position.pixels ==
+//          _scrollController.position.maxScrollExtent) {
+//        loadMorePost(nextLink);
+//        setState(() {});
+//
+////        print(nextLink);
+//      }
+//    });
+//  }
+//
+//  @override
+//  void dispose() {
+//    // TODO: implement dispose
+//    _scrollController.dispose();
+//    super.dispose();
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return ListView.builder(
+//        controller: _scrollController,
+//        itemCount: widget.product.length,
+//        itemBuilder: (context, index) {
+//          return Card(
+//            child: Column(
+//              mainAxisAlignment: MainAxisAlignment.start,
+//              mainAxisSize: MainAxisSize.min,
+//              crossAxisAlignment: CrossAxisAlignment.stretch,
+//              children: <Widget>[
+//                TopSection(
+//                  account: widget.product[index].accountName,
+//                  location: widget.product[index].businessLocation,
+//                ),
+////                PostSection(postImage: product[index].productPhoto),
+//                PostSection(postData: widget.product[index]),
+//                BottomSection(
+//                    callNo: widget.product[index].callNumber,
+//                    whatsapp: widget.product[index].whatsappNumber),
+//                PostDetails(caption: widget.product[index].caption),
+//              ],
+//            ),
+//          );
+//        });
+//  }
+//}
+//
+////Column(
+////children: <Widget>[
+////AppCategory(),
+////Expanded(
+////child: ListView.builder(
+////itemCount: imageList.length,
+////itemBuilder: (context, index) {
+////return SinglePost(imgUrl: imageList[index]);
+////},
+////),
+////),
+////],
+////),
+//
+///// This is the stateless widget that the main application instantiates.
+//class MyStatelessWidget extends StatelessWidget {
+//  MyStatelessWidget({Key key}) : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return DecoratedBox(
+//      decoration: BoxDecoration(
+//        color: Colors.white,
+//        border: Border.all(),
+//        borderRadius: BorderRadius.circular(20),
+//      ),
+//      child: Image.network(
+//        'https://example.com/image.jpg',
+//        loadingBuilder: (BuildContext context, Widget child,
+//            ImageChunkEvent loadingProgress) {
+//          if (loadingProgress == null) return child;
+//          return Center(
+//            child: CircularProgressIndicator(
+//              value: loadingProgress.expectedTotalBytes != null
+//                  ? loadingProgress.cumulativeBytesLoaded /
+//                      loadingProgress.expectedTotalBytes
+//                  : null,
+//            ),
+//          );
+//        },
+//      ),
+//    );
+//  }
+//}
