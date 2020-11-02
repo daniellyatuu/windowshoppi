629fff10192e22cc0a8ec1f962af29cde0197c9b
Lyatuu <daniellyatuu@gmail.com>
2020-11-02 12:22:52 +0300

    update

---
+++ lib/repository/LocalAPIClient.dart
@@ -0,0 +1,14 @@
+import 'package:windowshoppi/utilities/database_helper.dart';
+
+activeCountry() async {
+  final dbHelper = DatabaseHelper.instance;
+
+  var activeCountryData = await dbHelper.getActiveCountryFromUserTable();
+  if (activeCountryData == null) {
+    print('get country data');
+    // var _country = await _fetchCountry();
+    // return _country;
+  } else {
+    return activeCountryData;
+  }
+}
--- lib/repository/PostsAPIClient.dart
+++ lib/repository/PostsAPIClient.dart
@@ -4,19 +4,22 @@ import 'package:windowshoppi/models/models.dart';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 import 'dart:convert';
+import 'package:windowshoppi/repository/LocalAPIClient.dart';
 
 class PostsAPIClient {
-  static const baseUrl = SEARCH_POST + '?category=0&country=5';
+  static const baseUrl = SEARCH_POST + '?category=0';
   final http.Client httpClient;
   PostsAPIClient({@required this.httpClient}) : assert(httpClient != null);
 
   Future<List<Post>> getPosts(String keyword) async {
-    // print('step 3 : http call');
+    // get active country
+    var country = await activeCountry();
 
-    final searchUrl = '$baseUrl&keyword=$keyword';
-    print(searchUrl);
+    final searchUrl =
+        '$baseUrl&country=${country['id'].toString()}&keyword=$keyword';
     final response = await this.httpClient.get(searchUrl);
     print(response.statusCode);
+    // print(response.body);
     if (response.statusCode == 200) {
       return compute(parseProducts, response.body);
     } else {
